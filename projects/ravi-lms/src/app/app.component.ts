16830cc9608b5ac3d9d0a1da8cd3961eab663cfa
Ravi4245 <deshmukhravi121@gmail.com>
2025-05-29 18:18:19 +0530

    footer changes

--- projects/ravi-lms/src/app/app.component.ts
+++ projects/ravi-lms/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { FooterComponent } from './Courses/footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterModule],
+    imports: [RouterModule,FooterComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     template: `<router-outlet></router-outlet>`
