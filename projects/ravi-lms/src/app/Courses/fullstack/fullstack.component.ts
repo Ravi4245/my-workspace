09d885e8d993240d2bfa4d2a9ead9220218c1f18
SIDDHESHKADAM123 <siddheshkadam295@gmail.com>
2025-05-29 14:40:57 +0530

    changes in courses

--- projects/ravi-lms/src/app/Courses/fullstack/fullstack.component.ts
+++ projects/ravi-lms/src/app/Courses/fullstack/fullstack.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-fullstack',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './fullstack.component.html',
   styleUrl: './fullstack.component.css'
 })
