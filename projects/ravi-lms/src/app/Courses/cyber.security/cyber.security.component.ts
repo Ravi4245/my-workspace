194b1720e02ac4af784aae915b389c5675f43a90
Ravi4245 <deshmukhravi121@gmail.com>
2025-05-29 14:38:14 +0530

    cyber security

--- projects/ravi-lms/src/app/Courses/cyber.security/cyber.security.component.ts
+++ projects/ravi-lms/src/app/Courses/cyber.security/cyber.security.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cyber.security',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cyber.security.component.html',
   styleUrl: './cyber.security.component.css'
 })
